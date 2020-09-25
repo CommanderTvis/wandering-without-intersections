5485af5742baaed02ec911b2a53a5c690269cd4f
Iaroslav Postovalov <38042667+CommanderTvis@users.noreply.github.com>
2020-09-25 13:34:57 +0700

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -3,8 +3,8 @@ from typing import List
 from matplotlib.pyplot import *
 from main import solution
 
-_n_times: int = 300
-_x: range = range(1, 101, 3)
+_n_times: int = 1000
+_x: range = range(1, 101, 5)
 _y: List[float] = list(map(lambda x: solution(_n_times, x), _x))
 print("data is ready")
 plot(_x, _y)
